61f08ccefad51fa080bb143d0867573cd320e7b5
fullstack82 <fullstack8210@gmail.com>
2020-11-27 22:45:27 +0100

    llamadas

--- whatsapp/src/Llamadas/Llamadas.js
+++ whatsapp/src/Llamadas/Llamadas.js
@@ -2,6 +2,7 @@ import Compra from './Compra';
 import Hashtag from './Hashtag';
 import Contador from './Contador';
 import Eurocalc from './Eurocalc';
+import Login from './Login';
 
 function Llamadas() {
   return (
@@ -10,6 +11,7 @@ function Llamadas() {
       <Compra />
       <Hashtag />
       <Eurocalc />
+      <Login />
     </>
   );
 }
