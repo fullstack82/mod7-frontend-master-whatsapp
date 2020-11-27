f69a0a355b1460af79c506b69b9711d684e302a2
fullstack82 <fullstack8210@gmail.com>
2020-11-27 22:36:04 +0100

    login

---
+++ whatsapp/src/Llamadas/Login.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './Login.css'
+
+
+const secretUser = 'Anonimo'
+const secretPass = '1234'
+
+
+function Login() {
+    const [message, setMessage] = useState('')
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+    const handleSubmit = e => {
+        e.preventDefault()
+        if(user === secretUser && password === secretPass) {
+            setMessage('Bienvenido')
+        }else{
+            setMessage('Usuario o contraseña incorrecto')
+        }
+    }
+
+    return(
+        <form className="login" onSubmit= {handleSubmit}>
+            <input value={user} onChange={e => setUser(e.target.value)} />
+            <input value={password} onChange={e => setPassword(e.target.value)} />
+            <button>Iniciar sesión</button>
+            <div>{message}</div>
+        </form>
+    )
+}
+
+
+export default Login
